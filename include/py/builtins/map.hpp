3d37471edf3813392a9fc566d4e3bf1f7828bd5d
Shogo Sawai <shogo.sawai@gmail.com>
2019-06-19 13:29:05 +0900

    refactor map

--- include/py/builtins/map.hpp
+++ include/py/builtins/map.hpp
@@ -15,23 +15,13 @@ namespace py {
 
 template <class TFunc, class TupleType>
 class imap_iterator {
-    // using element_type = decltype(std::declval<TFunc>()(*(std::declval<array_iterator_type>())));
-
  public:
     imap_iterator(TFunc &func_, TupleType&& iter_) : func(func_), iter(iter_) {}
-    // imap_iterator(const imap_iterator &i) : func(i.func), iter(i.iter) {}
-    // imap_iterator &operator = (const imap_iterator &i) { iter = i.iter; }
-    // imap_iterator operator++(int) {
-    //     imap_iterator tmp(*func, iter);
-    //     ++iter;
-    //     return tmp;
-    // }
     imap_iterator &operator++() {
         internal::tuple_pincrement(iter);
         return *this;
     }
     auto operator*() const { return internal::tuple_apply_iterators(func, iter); }
-    // auto operator->() const { return (*func)(*iter); }
     bool operator==(const imap_iterator &rhs) const { return internal::tuple_equal_or(iter, rhs.iter); }
     bool operator!=(const imap_iterator &rhs) const { return not internal::tuple_equal_or(iter, rhs.iter); }
 
@@ -41,95 +31,29 @@ class imap_iterator {
 };
 
 
-template <class TFunc, class ...Tarrays>
-class imap_lvalue {
+template <class TFunc, class TupleType>
+class imap {
  public:
-    // using array_type = typename std::remove_const<typename std::remove_reference<Tarray>::type>::type;
-    // using array_iterator_type = decltype(std::declval<const Tarray>().begin());
-
- public:
-    imap_lvalue(TFunc func_, Tarrays&...args) : func(func_), arrays(std::forward_as_tuple(args...)) {}
+    imap(TFunc func_, TupleType &&args_) : func(func_), args(args_) {}
     auto begin() {
-        auto iters = internal::tuple_begin(arrays);
+        auto iters = internal::tuple_begin(args);
         return imap_iterator<TFunc, decltype(iters)>(func, std::move(iters));
     }
     auto end() {
-        auto iters = internal::tuple_end(arrays);
+        auto iters = internal::tuple_end(args);
         return imap_iterator<TFunc, decltype(iters)>(func, std::move(iters));
     }
-    // auto end() const { return internal::tuple_end(arrays); }
-
-    // imap_iterator<TFunc, decltype(begin(std::declval<std::tuple<Tarrays&...>>()))> begin() const {
-    //     return imap_iterator<TFunc, decltype(begin(arrays))>(func, begin(arrays));
-    // }
-    // imap_iterator<TFunc, decltype(end(std::declval<std::tuple<Tarrays&...>>()))> end() const {
-    //     return imap_iterator<TFunc, decltype(end(arrays))>(func, end(arrays));
-    // }
-    // imap_iterator<TFunc, array_iterator_type> cbegin() const { return begin(); }
-    // imap_iterator<TFunc, array_iterator_type> cend() const { return end(); }
 
  public:
     TFunc func;
-    std::tuple<Tarrays&...> arrays;
+    TupleType args;
 };
 
-template <class TFunc, class ...Tarrays>
-class imap_rvalue {
- public:
-    // using array_type = typename std::remove_const<typename std::remove_reference<Tarray>::type>::type;
-    // using array_iterator_type = decltype(std::declval<const Tarray>().begin());
-
- public:
-    imap_rvalue(TFunc func_, const Tarrays &&...ar) : func(func_), arrays(ar...) {}
-    auto begin() {
-        auto iters = internal::tuple_begin(arrays);
-        return imap_iterator<TFunc, decltype(iters)>(func, std::move(iters));
-    }
-    auto end() {
-        auto iters = internal::tuple_end(arrays);
-        return imap_iterator<TFunc, decltype(iters)>(func, std::move(iters));
-    }
-    // imap_iterator<TFunc, array_iterator_type> begin() const {
-    //     return imap_iterator<TFunc, array_iterator_type>(func, array.begin());
-    // }
-    // imap_iterator<TFunc, array_iterator_type> end() const {
-    //     return imap_iterator<TFunc, array_iterator_type>(func, array.end());
-    // }
-    // imap_iterator<TFunc, array_iterator_type> cbegin() const { return begin(); }
-    // imap_iterator<TFunc, array_iterator_type> cend() const { return end(); }
-
- public:
-    TFunc func;
-    std::tuple<Tarrays...> arrays;
-    // array_type array;
-};
-
-// template <class TFunc, class ...Tarrays>
-// imap_lvalue<TFunc, Tarrays...> map(TFunc func_, const Tarrays&... ar) {
-//     return imap_lvalue<TFunc, Tarrays...>(func_, ar...);
-// }
-template <class TFunc, class ...Tarrays>
-auto map(TFunc &&func_, Tarrays&... ar) {  // NOLINT
-    return imap_lvalue<TFunc, Tarrays...>(func_, ar...);
+template <class TFunc, class ...Args>
+auto map(TFunc &&func_, Args &&... args) {
+    auto t = std::tuple_cat(internal::tuple_intelligent_forward<Args>(args)...);
+    return imap<TFunc, decltype(t)>(func_, std::move(t));
 }
-template <class TFunc, class ...Tarrays>
-auto map(TFunc &func_, Tarrays&... ar) {  // NOLINT
-    return imap_lvalue<TFunc&, Tarrays...>(func_, ar...);
-}
-
-template <class TFunc, class ...Tarrays>
-auto map(TFunc &&func_, Tarrays&&... ar) {
-    return imap_rvalue<TFunc, Tarrays...>(func_, std::move(ar...));
-}
-template <class TFunc, class ...Tarrays>
-auto map(TFunc &func_, Tarrays&&... ar) {
-    return imap_rvalue<TFunc&, Tarrays...>(func_, std::move(ar...));
-}
-// template <class TFunc, class IteratorType>
-// Iterable<imap_iterator<TFunc, IteratorType>> map(const TFunc &func_, IteratorType begin_, IteratorType end_) {
-//     return Iterable<imap_iterator<TFunc, IteratorType>>(imap_iterator<TFunc, IteratorType>(func_, begin_),
-//                                                         imap_iterator<TFunc, IteratorType>(func_, end_));
-// }
 
 }  // namespace py
 
